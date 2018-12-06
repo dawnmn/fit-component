528977b250c263c38df521de02b188a3311ac607
dawnmn <dawnmn@126.com>
2018-12-06 15:03:37 +0800

    增加config

---
+++ src/fit/common/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright   时刻共享
+ * @author      dawn
+ * @since       2018/12/6 13:57
+ */
+
+
+namespace fit\common;
+
+
+class Config
+{
+    const DEFAULT_DATETIME = '2017-01-01 00:00:00';
+}
